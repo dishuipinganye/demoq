eb0b4666f1a57c3664dde1dd0b561d7a9193d952
dishuipinganye <2365423799@qq.com>
2018-10-15 17:18:20 +0800

    my分支提交了

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//嘎嘎嘎
+@SpringBootApplication//嘎嘎嘎我是my分支
 public class DemoApplication {
 
 	public static void main(String[] args) {
