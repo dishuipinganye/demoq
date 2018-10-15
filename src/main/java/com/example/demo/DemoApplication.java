dfd9f6bcfed661d262905ad1541f57aba276769d
dishuipinganye <2365423799@qq.com>
2018-10-15 10:10:22 +0800

    钱钱钱

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//和你说
+@SpringBootApplication//和你说看吧
 public class DemoApplication {
 
 	public static void main(String[] args) {
