2cb216d0454a8b260ccff4cb817d5d42c9f2ef19
dishuipinganye <2365423799@qq.com>
2018-10-15 10:19:54 +0800

    好好干

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//和你说看吧哈哈空间
+@SpringBootApplication//和你说看吧哈哈空间官方
 public class DemoApplication {
 
 	public static void main(String[] args) {
