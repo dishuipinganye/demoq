46669ec29aa2a3cb0a85f5f43d7bb07def9110bc
dishuipinganye <2365423799@qq.com>
2018-10-15 10:26:31 +0800

    宝宝哈哈

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication//宝宝
+@SpringBootApplication//宝宝哈哈
 public class DemoApplication {
 
 	public static void main(String[] args) {
